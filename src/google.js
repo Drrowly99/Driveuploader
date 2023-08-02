1e8bb50fea745574eb04eac36946f553db0d4ca6
drrowly99 <drrowly99@gmail.com>
2023-08-02 06:55:34 +0100

    update

--- src/google.js
+++ src/google.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const request = require("request");
 const {google} = require('googleapis');
 const path = require('path');
 const process = require('process');
@@ -14,12 +15,18 @@ const oauth2Client = new google.auth.OAuth2(
     process.env.YOUR_CLIENT_SECRET,
     process.env.YOUR_REDIRECT_URL
   );
+
+  const oauth3Client = new google.auth.OAuth2(
+    process.env.YOUR_CLIENT_ID,
+    process.env.YOUR_CLIENT_SECRET,
+    process.env.YOUR_REDIRECT_URL
+  );
   
   // set auth as a global default
 
 
   try{
-    const cred = fs.readFileSync("creds.json");
+    const cred = fs.readFileSync("cred.json");
     oauth2Client.setCredentials(JSON.parse(cred)); // should be set as json
   } catch(err) {
     console.log("no creds found")
@@ -28,31 +35,84 @@ const oauth2Client = new google.auth.OAuth2(
   module.exports = app.get('/auth', (req, res) => {
     const url = oauth2Client.generateAuthUrl({
         access_type: "offline",
-        scope: ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/drive"],
+        scope: ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/drive"],
     });
     res.redirect(url);
 });
 
+module.exports = app.get('/user', async (req, res) => {
+    oauth3Client.setCredentials({access_token: process.env.ACCESS_TOKEN}); 
+    const oath_user = google.oauth2({
+        version: 'v2',
+        auth: oauth3Client
+      });
+        let { data } = await oath_user.userinfo.get();    // get user info
+      console.log(data.id);
+      console.log(data.email);
+      console.log('second wave');
+
+      oauth3Client.on('tokens', (tokens) => {
+        if (tokens.refresh_token) {
+          // store the refresh_token in my database!
+          console.log(tokens.access_token);
+        }
+      });
+
+});
+
+module.exports = app.get('/refresh', async (req, res) => {
+    var tok;
+    var read = fs.readFileSync('cred.json');
+    var json = JSON.parse(read);
+    console.log(json);
+
+    oauth2Client.setCredentials(json); 
+
+      oauth2Client.refreshAccessToken((err, tokens) => {
+        // your access_token is now refreshed and stored in oauth2Client
+        // store these new tokens in a safe place (e.g. database)
+        console.log(tokens.access_token)
+        tok = tokens.access_token
+        res.send(`success:${tok}`)
+        fs.writeFileSync("cred.json", JSON.stringify(tokens));
+      });
+
+    //   refreshAccessToken()
+    
+});
+
 module.exports = app.get('/redirect', async (req, res) => {
     console.log('redirect')
-    
-    let str = "drrowly99@gmail.com"
-    let email_ = str.substring(0, str.length - 10);
-
     const {code} = req.query;
     const {tokens} = await oauth2Client.getToken(code);
-
-    // let tok = [{'email':email_,'data':tokens}]
-    // console.log(tok)
-    // var read = fs.readFileSync('cred.json');
-    // var json = JSON.parse(read);
-    // json.push(tok)
     
     oauth2Client.setCredentials(tokens);
-    fs.writeFileSync("cred.json", JSON.stringify(tokens));
-// console.log('finally'+tok[2])
 
-    res.send(`Success ${tokens.access_token} and refresh = ${tokens.refresh_token} and scope = ${tokens.scope}`);
+    fs.writeFileSync("cred.json", JSON.stringify(tokens));
+
+    // let { data } = await oath_user.userinfo.get();    // get user info
+    console.log('first wave');
+    console.log(tokens.access_token)
+
+
+    oauth2Client.on('tokens', (tokens) => {
+    if (tokens.refresh_token) {
+      // store the refresh_token in my database!
+      console.log(tokens.refresh_token);
+    }
+    console.log(tokens.access_token);
+  });
+    //   refreshAccessToken('null', tokens)
+    //   oauth2Client.refreshAccessToken((err, tokens) => {
+    //     // your access_token is now refreshed and stored in oauth2Client
+    //     // store these new tokens in a safe place (e.g. database)
+    //     console.log(tokens.access_token)
+    //   });
+
+      res.redirect('http://localhost:3000/up')
+
+      res.send(`Success ${tokens.access_token} and refresh = ${tokens.refresh_token} and scope = ${tokens.scope}`);
+
 });
 
 module.exports = app.get('/redirects', async (req, res) => {
@@ -79,6 +139,47 @@ module.exports = app.get('/redirects', async (req, res) => {
 
 
 module.exports = app.post('/upload/', uploadMiddleware, async (req, res) => {
+
+    // 1. Retrieve session for resumable upload.
+request(
+    {
+      method: "POST",
+      url:
+        "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name: "sample.png", mimeType: "image/png" })
+    },
+    (err, res) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+  
+      // 2. Upload the file.
+      request(
+        {
+          method: "PUT",
+          url: res.headers.location,
+          headers: { "Content-Range": `bytes 0-${fileSize - 1}/${fileSize}` },
+          body: fs.readFileSync(filename)
+        },
+        (err, res, body) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log(body);
+        }
+      );
+    }
+  );
+
+})
+
+module.exports = app.post('/uploads/', uploadMiddleware, async (req, res) => {
       // Handle the uploaded files
         const files = req.files;
         var numCallbackRuns = 0;
@@ -147,6 +248,8 @@ async function generatePublicUrl(fileIds) {
             requestBody: {
                 role: 'reader',
                 type: 'anyone'
+                // type: 'user',
+                // emailAddress: 'youremail@gmail.com'
             }
         })
 
@@ -173,7 +276,7 @@ async function uploadFile(data, type, filename_){
               });
               
     const sometext = 'mono is good'
-    const folderId = '1pLyg3j9a2T1IksJr8XMrY9evddCjLSgS';
+    const folderId = data;
     console.log('CONFIRM FIRST FOLDER Id:',folderId);
     if(folderId !== 'none'){
         try{
